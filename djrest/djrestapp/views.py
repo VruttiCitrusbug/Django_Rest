39f4fb2b828e66ce8a5250d95cb6fd4126d52bf2
unknown <vrutti.citrubsug@gmail.com>
2022-11-02 11:20:21 +0530

    updated

--- djrest/djrestapp/views.py
+++ djrest/djrestapp/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+# from rest_framework.pagination import PageNumberPagination
 # from rest_framework.authtoken.views import obtain_auth_token
 # from rest_framework.permissions import IsAuthenticated
 # class testview(APIView):
@@ -18,25 +19,6 @@ from rest_framework.permissions import IsAuthenticated
 #         qs=User.objects.all()
 #         ser=seruser(qs,many=True)
 #         return Response(ser.data)
-class createview(APIView):
-    def post(self,request):
-        ser=seruser(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            u=User.objects.get(username=ser.data['username'])
-            token, created = Token.objects.get_or_create(user=u)
-            return Response({'status':403,'payload':ser.data,'token':token.key})
-        else:
-            return Response({'status':403,'errors':ser.errors})
-    # from djrestapp import views
-    # path('',views.testview.as_view(),name="testview")
-class authview(ListAPIView):
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
-    def get(self,request):
-        queryset=User.objects.all()
-        ser=seruser(queryset,many=True)
-        return Response(ser.data)
 class genview(ObtainAuthToken):
 
     def post(self, request):
@@ -53,3 +35,23 @@ class genview(ObtainAuthToken):
                 'error': "user not found"
             }
         )
+class createview(APIView):
+    def post(self,request):
+        ser=seruser(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            u=User.objects.get(username=ser.data['username'])
+            token, created = Token.objects.get_or_create(user=u)
+            return Response({'status':403,'payload':ser.data,'token':token.key})
+        else:
+            return Response({'status':403,'errors':ser.errors})
+    # from djrestapp import views
+    # path('',views.testview.as_view(),name="testview")
+class authview(ListAPIView):
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    # pagination_class=PageNumberPagination
+    def get(self,request):
+        queryset=User.objects.all()
+        ser=seruser(queryset,many=True)
+        return Response(ser.data)
